07cc639a9cb539b53519b8a8c9e5094eda1f44af
r <ssssp@hotmail.com>
2016-07-15 12:06:34 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/msg', function(req, res) {
+  //Find
+  Chat.find({
+    'room': req.query.room.toLowerCase()
+  }).exec(function(err, msgs) {
+    //Send
+    res.json(msgs);
+  });
+});
+
+io.on('connection', function(socket){
+    
+  var defaultRoom = 'general';
+  var rooms = ["General", "other"];
+    var hg="fdsf";
+    
+  socket.emit('chat message', 'Rooms :' +rooms);
+//    io.emit('chat message', hg);
+
+    
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
