fc45a9e1b196ac2c4d58e0d5b48db48f6cf72258
R <ssssp@hotmail.com>
2016-07-31 20:45:45 -0400

    Changes

--- clientOne.js
+++ clientOne.js
@@ -14,14 +14,16 @@
 //    a:[0,1,2,3,4,5,6,7,8,9]
 //}
 
-exports.greeting = function(){
-        console.log("Hey, how are you!");
-    }
- 
-exports.printDate = function(){
-        console.log(Date());        
-    }
- 
-exports.pi = 3.14;
- 
-exports.a = [0,1,2,3,4,5,6,7,8,9];
+//exports.greeting = function(){
+//        console.log("Hey, how are you!");
+//    }
+// 
+//exports.printDate = function(){
+//        console.log(Date());        
+//    }
+// 
+//exports.pi = 3.14;
+// 
+//exports.a = [0,1,2,3,4,5,6,7,8,9];
+
+alert("hola");
--- index.js
+++ index.js
@@ -6,25 +6,39 @@ var logon=false;
 var logged_user;
 var defaultRoom = 'general';
 var rooms = ["General", "Other", "Secret"];
+var domains = ["team_maju","other_team"];
+
 
 
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-//    res.sendFile(__dirname + '/clientOne.js');
 });
 
 
 io.on('connection', function(socket){
     
+//Search for available rooms and contacts DB
+    
   socket.emit('available_rooms', rooms);
     
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-    
     socket.on('message', function(msg){
-    io.emit('message', msg);
+        
+        var msg_domain=msg.domain;
+        var msg_group=msg.group;
+        
+//        if (domains.indexOf(msg_domain)<=0)
+//            {
+//                alert("Error, domain doesn't exist");
+//            }
+//        if (msg.receiver.length<=0)
+//            {
+//                alert("Error, user doesn't exist");
+//            }
+        
+        
+        io.emit(msg.receiver, msg.message.text);
+        
     });
     
     socket.on('session', function(msg){
